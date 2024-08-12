70f3b21f55cc6a4fad0bdf08c2f49edd36366924
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2024-08-12 20:08:29 -0300

    Update Bubble-Sort.py
    fiz uma breve descrição sobre este algoritmo e como ele funciona

--- Algoritimos/Bubble-Sort.py
+++ Algoritimos/Bubble-Sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(arr):
-    n = len(arr) 
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+    n = len(arr) #reserve o valor do tamanho do array em n
+    for i in range(n): #percorra n de i em i
+        for j in range(0, n-i-1): #percorra j, iniciando em 0 e terminando em n-1-1 = 5
+            if arr[j] > arr[j+1]: #se o array j for maior que o array j+1
+                arr[j], arr[j+1] = arr[j+1], arr[j] # troque os arrays de posição, ou lugar
+    return arr # senao retorne o array
 
 # Exemplo de uso
 arr = [64, 34, 25, 12, 22, 11, 90]
